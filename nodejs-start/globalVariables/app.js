eaf03a5748659df256c1e75c3aee848e3b27e43b
Krupka <krupka.ua@gmail.com>
2022-03-10 16:17:57 +0200

    backend | nodejs-start - globalVariables

---
+++ nodejs-start/globalVariables/app.js
@@ -0,0 +1,7 @@
+console.log('__dirname', __dirname);
+console.log('__filename', __filename);
+console.log('=== global ===', global);
+console.log('=== performance ===', performance);
+console.log('=== module ===', module);
+console.log('=== exports ===', exports);
+console.log('=== require ===', require);
