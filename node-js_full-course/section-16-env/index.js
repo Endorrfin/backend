5ef8483cdd4aa059cbf6b15b6c5fb8c3ed1df007
krupka <krupka.ua@gmail.com>
2023-06-13 10:07:49 +0300

    node-js | full course | section 16 environment variables

---
+++ node-js_full-course/section-16-env/index.js
@@ -0,0 +1,11 @@
+// require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env);
+
+console.log(require('dotenv'));
+
+console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_URL);
