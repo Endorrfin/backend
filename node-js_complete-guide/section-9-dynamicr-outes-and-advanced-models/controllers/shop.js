f292a4f9371618e9c70aa242969638cf313f5b5c
Krupka <krupka.ua@gmail.com>
2025-08-06 20:20:51 +0300

    node.js complete-guide | dynamic routes & advanced models

--- node-js_complete-guide/section-9-dynamicr-outes-and-advanced-models/controllers/shop.js
+++ node-js_complete-guide/section-9-dynamicr-outes-and-advanced-models/controllers/shop.js
@@ -11,7 +11,6 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
@@ -21,7 +20,7 @@ exports.getProduct = (req, res, next) => {
       path: '/products'
     });
   });
-}
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -34,9 +33,23 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
 };
 
@@ -46,6 +59,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getOrders = (req, res, next) => {
