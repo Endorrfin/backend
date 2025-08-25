5c9b19d5ca43a740e7d0dc33b15ffae7d1f7225f
Krupka <krupka.ua@gmail.com>
2025-08-25 16:28:30 +0300

    node.js advanced-concept | enhancing-node-performance

---
+++ node-js_advanced-concepts/topic-02_enhancing-node-performance/clustering.js
@@ -0,0 +1,32 @@
+process.env.UV_THREADPOOL_SIZE = 1
+
+const cluster = require('cluster');
+
+
+// Is the file being executed in master mode?
+if(cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child (slave) mode
+  cluster.fork();
+
+} else {
+  // Im a child, Im doing to act like a server and do nothing else
+  const express = require('express');
+  const crypto = require('crypto');
+  const app = express();
+
+
+
+  app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 400000, 512, 'sha512', () => {
+      res.send('Hi there 🙋‍♂️');
+    });
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('This was fast! 🏎️');
+  })
+
+  app.listen(3000);
+}
+
+
